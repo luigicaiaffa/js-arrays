47eb029637edfac02e5e36574b1a1dc833930c9d
luigicaiaffa <luigicaiaffa8@gmail.com>
2024-10-16 15:45:41 +0200

    array ex 10

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -42,4 +42,5 @@ console.log(teachersString);
 
 // 10. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+const isTeachersEmpty = teachers.length === 0;
+console.log(isTeachersEmpty);
